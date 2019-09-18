3a613cce9ebcf0f5dbaf405eec472c69a41dca9a
Jesse Longacre <jesse@jlte.ch>
2019-09-18 09:08:59 -0700

    Add plugin and dialogs.

---
+++ dialogs/ckbutton.js
@@ -0,0 +1,182 @@
+CKEDITOR.dialog.add('ckbutton', function (editor) {
+  var lang = editor.lang.ckbutton;
+  var styles = [];
+  var buttonStyles = editor.config.buttonStylesSet;
+  for (var i = 0; i < buttonStyles.length; i++) {
+    styles.push([buttonStyles[i].name, i])
+  }
+
+  return {
+    title: 'Custom Button',
+    minWidth: 500,
+    minHeight: 150,
+    resizable: false,
+    contents: [
+      {
+        id: 'info',
+        label: lang.tabInfo,
+        accessKey: 'I',
+        elements: [
+          {
+            type: "hbox",
+            widths: ["50%", "100%"],
+            children: [
+              {
+                id: "styles",
+                type: "select",
+                label: lang.buttonStylesLabel,
+                items: styles,
+                setup: function (widget) {
+                  this.setValue(widget.data.styles || '');
+                },
+                commit: function (widget) {
+                  widget.setData('styles', this.getValue());
+                }
+              }
+            ]
+          },
+          {
+            type: "hbox",
+            widths: ["50%", "50%"],
+            children: [
+              {
+                id: 'text',
+                type: 'text',
+                width: '200px',
+                required: true,
+                label: lang.buttonTextLabel,
+                setup: function (widget) {
+                  this.setValue(widget.data.text || 'A Button');
+                },
+                commit: function (widget) {
+                  widget.setData('text', this.getValue());
+                }
+              },
+              {
+                id: 'href',
+                type: 'text',
+                width: '200px',
+                required: true,
+                label: lang.buttonUrlLabel,
+                setup: function (widget) {
+                  this.setValue(widget.data.href || '#');
+                },
+                commit: function (widget) {
+                  widget.setData('href', this.getValue());
+                }
+              }
+            ]
+          }
+        ]
+      },
+      {
+        id: 'target',
+        label: lang.tabTarget,
+        elements: [
+          {
+            id: "target",
+            type: "select",
+            label: lang.buttonTargetLabel,
+            items: [
+              ['Not Set', ''],
+              ['Frame', "frame"],
+              ['Popup', "popup"],
+              ['New Window (_blank)', "_blank"],
+              ['Topmost Window (_top)', "_top"],
+              ['Same Window (_self)', "_self"],
+              ['Parent Window (_parent)', "_parent"]
+            ],
+            setup: function (widget) {
+              this.setValue(widget.data.target || '');
+            },
+            commit: function (widget) {
+              widget.setData('target', this.getValue());
+            }
+          }
+        ]
+      },
+      {
+        id: 'icons',
+        label: lang.tabIcons,
+        elements: [
+          {
+            type: "hbox",
+            widths: ["50%", "50%"],
+            children: [
+              {
+                type: 'vbox',
+                children: [
+                  {
+                    type: 'html',
+                    html: '<strong>Bootstrap Glyphicon</strong>' +
+                      '<p><a href="http://getbootstrap.com/components/#glyphicons" target="_blank" style="padding: 0px; vertical-align: top;">List of Icons</a></p><br/>' +
+                      '<p>e.g. <em>glyphicon-pencil</em></p>'
+                  },
+                  {
+                    id: 'bsiconleft',
+                    type: 'text',
+                    width: '150px',
+                    label: 'Left Icon',
+                    setup: function (widget) {
+                      this.setValue(widget.data.bsiconleft || '');
+                    },
+                    commit: function (widget) {
+                      widget.setData('bsiconleft', this.getValue());
+                    }
+                  },
+                  {
+                    id: 'bsiconright',
+                    type: 'text',
+                    width: '150px',
+                    label: 'Right Icon',
+                    setup: function (widget) {
+                      this.setValue(widget.data.bsiconright || '');
+                    },
+                    commit: function (widget) {
+                      widget.setData('bsiconright', this.getValue());
+                    }
+                  }
+                ]
+              },
+              {
+                type: 'vbox',
+                children: [
+                  {
+                    type: 'html',
+                    html: '<strong>Font Awesome</strong>' +
+                      '<p><a href="http://fortawesome.github.io/Font-Awesome/cheatsheet/" target="_blank" style="padding: 0px; vertical-align: top;">List of Icons</a></p><br/>' +
+                      '<p>e.g. <em>fa-arrow-right</em></p>'
+                  },
+                  {
+                    id: 'faiconleft',
+                    type: 'text',
+                    width: '150px',
+                    label: 'Left Icon',
+                    setup: function (widget) {
+                      this.setValue(widget.data.faiconleft || '');
+                    },
+                    commit: function (widget) {
+                      widget.setData('faiconleft', this.getValue());
+                    }
+                  },
+                  {
+                    id: 'faiconright',
+                    type: 'text',
+                    width: '150px',
+                    label: 'Right Icon',
+                    setup: function (widget) {
+                      this.setValue(widget.data.faiconright || '');
+                    },
+                    commit: function (widget) {
+                      widget.setData('faiconright', this.getValue());
+                    }
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  };
+});
